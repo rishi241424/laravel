354d3d4dafba2aaef8173a5d8299aeb2108cf6df
rishi241424 <rishi.hardia2414@gmail.com>
2020-07-08 15:27:19 +0530

    New Title  field added in Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,20 @@
                         @csrf
 
                         <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Title</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus>
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
